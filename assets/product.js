46ac4569ec4054db019fe82ba11b2aa9da62bad3
Swathi1003-sri <swathiarchunan103@gmail.com>
2025-06-19 15:04:34 +0100

    Add files via upload

---
+++ assets/product.js
@@ -0,0 +1,401 @@
+var total_product=[
+    {
+        id:1,
+        img:"https://justbakedcake.com/wp-content/uploads/2020/11/Vanilla-Cake-photo.jpg",
+        name:"Vanilla",
+        price:300
+
+    },
+    {
+        id:2,
+        img:"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh0_tbrJhFqRBvlE7U2ICxmKMfmAS80QxP_naYKScnbLyK_zrqKmX7dRX5KbrHRUV0zhJE7OcXxrbn-phcibPiCD5rG5_S2dBiJrBfKgB1SxheGEdBFd_VfFOqFR9BPQqnp_ZIeWgtpX8L6/s1600/DSC07710.JPG",
+        name:"Black Forest ",
+        price:600
+
+    },
+    {
+        id:3,
+        img:"https://yourfoodfantasy.com/wp-content/webpc-passthru.php?src=https://yourfoodfantasy.com/wp-content/uploads/2021/03/Eggless-homemade-rasmalai-cake.jpg&nocache=1",
+        name:"Rasmalai ",
+        price:600
+
+    },
+    {
+        id:4,
+        img:"https://teakandthyme.com/wp-content/uploads/2024/04/strawberry-chocolate-cake-DSC_9202-1x1-1200.jpg",
+        name:"Strawberry chocolate",
+        price:400
+
+    },
+    {
+        id:5,
+        img:"https://www.theflowerspoint.com/data/cache/images/cakes/cake%20by%20flavour/chocolate%20cake/Premium%20Cakes/butter-scotch-01-441x441.png",
+        name:"Butter scotch",
+        price:500
+
+    },
+    {
+        id:6,
+        img:"https://stylesweet.com/wp-content/uploads/2022/06/ChocolateCakeForTwo_Featured.jpg",
+        name:"Chocolate",
+        price:350
+
+    },
+    {
+        id:7,
+        img:"https://www.orderyourchoice.com/124930-large_default/black-currant-cake-.jpg",
+        name:"Black current",
+        price:550
+
+    },
+    {
+        id:8,
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEQymRDT99RzRA55-NpsYg0yeVMZ5WDu3rI58ROpWTW0VQou-YlngVpw70n2boeCZapvE&usqp=CAU",
+        name:"White forest",
+        price:600
+
+    },
+    {
+        id:9,
+        img:"https://thecheesecakeboutique.com/cdn/shop/files/9inch_HB_strawb_1_aa87ea7d-ea87-4f0e-96dc-e2a18d705e33.jpg?v=1729473772&width=480",
+        name:"Cheese cake",
+        price:450
+
+    },
+    {
+        id:10,
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6n25MSyONRqgVKeu9nq6im3npTPZUAlXoZg&s",
+        name:"Red velvet",
+        price:"500"
+
+    },
+    {
+        id:11,
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlovc-_5C8elliubwLI3uS4432IFwRy8P0Jw&s",
+        name:"Fruit cake",
+        price:600
+
+    },
+    {
+        id:12,
+        img:"https://vikalinka.com/wp-content/uploads/2022/03/Carrot-Cake-14-Edit.jpg",
+        name:"Carrot cake",
+        price:400
+
+    },
+    {
+        id:13,
+        img:"https://recipesblob.oetker.ca/assets/0dcacdf7af944d4bb73c9dc0deaa8004/750x910/coconut-cake-one-slice-missing-11.webp",
+        name:"Coconut cake",
+        price:450
+
+    },
+    {
+        id:14,
+        img:"https://recipesbyclare.com/cdn-cgi/image/fit=cover,width=1280,height=1280,format=auto/assets/images/1737383252128-k7bcpz7m.webp",
+        name:"Banana cake",
+        price:350
+
+    },
+    {
+        id:15,
+        img:"https://yummycake.in/wp-content/uploads/2022/03/p-sunshine-mango-cake-1-kg-135357-m.webp",
+        name:"Mango cake",
+        price:500
+
+    },
+    {
+        id:16,
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPYmOzS55HtKUG7ttm3gPlO3J1c5fuwRPJLg&s",
+        name:"caramel cake",
+        price:350
+
+    },
+    {
+        id:17,
+        img:"https://media.istockphoto.com/id/635743170/photo/cupcakes.jpg?s=612x612&w=0&k=20&c=D1FIKePUI8XejJlz6mP3aNlb5P1gCiY112R7A_Jtydk=",
+        name:"Cup cake",
+        price:30
+
+    },
+    {
+        id:18,
+        img:"https://ocakes.in/storage/app/public/images/item/item-65b240fe364ef.webp",
+        name:"Dream cake",
+        price:800
+
+    },
+    {
+        id:19,
+        img:"https://cakebee.in/cdn/shop/products/HoneyCake_1.jpg?v=1676568198",
+        name:"Honey cake",
+        price:550
+
+    },
+    {
+        id:20,
+        img:"https://bakerpedia.com/wp-content/uploads/38752431_m.jpg",
+        name:"Wheat bread",
+        price:45
+
+    },
+    {
+        id:21,
+        img:"https://preppykitchen.com/wp-content/uploads/2023/10/Potato-Bread-Recipe-Card.jpg",
+        name:"Potato bread",
+        price:100
+
+    },
+    {
+        id:22,
+        img:"https://floursandfrostings.com/wp-content/uploads/2018/03/IMG_20180306_084523_451.jpg",
+        name:"Veg puff",
+        price:15
+
+    },
+    {
+        id:23,
+        img:"https://www.licious.in/blog/wp-content/uploads/2022/01/shutterstock_1450591601.jpg",
+        name:"Egg puff",
+        price:20
+
+    },
+    {
+        id:24,
+        img:"https://ministryofcurry.com/wp-content/uploads/2019/10/kathi-roll_.jpg",
+        name:"Paneer Roll",
+        price:20
+
+    },
+    {
+        id:25,
+        img:"https://www.shutterstock.com/image-photo/selective-focus-indian-food-paneer-600nw-2441817529.jpg",
+        name:"Chicken Roll",
+        price:30
+
+    },
+    {
+        id:26,
+        img:"https://img.freepik.com/free-photo/juicy-delicious-meat-cutlets-dark-table-russian-cuisine_2829-7278.jpg?uid=R191391205&ga=GA1.1.59354729.1741842120&semt=ais_hybrid&w=740",
+        name:"Cutlet",
+        price:50
+    },
+    {
+        id:27,
+        img:"https://img.freepik.com/free-photo/samsa-samosas-with-meat_658428-358.jpg?uid=R191391205&ga=GA1.1.59354729.1741842120&semt=ais_hybrid&w=740",
+        name:"Chicke Samosa",
+        price:25
+    },
+    {
+        id:28,
+        img:"https://img.freepik.com/free-photo/julienne-mushroom-cream-chicken-cheese-sour-cream-top-green-salad-side-view_141793-3754.jpg?uid=R191391205&ga=GA1.1.59354729.1741842120&semt=ais_hybrid&w=740",
+        name:"Bread Ombelet",
+        price:45
+    }
+]
+
+
+function submit_fun(event){
+    event.preventDefault()
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+
+    if(user === "Swathi" && pass === "Abcde@12345"){
+        window.location.href = "./assets/addtocart.html"
+    }
+    else{
+        res = "INVALID"
+    }
+    document.getElementById("result1").innerHTML=res;
+}
+function toggleIcon(){
+     var  passw = document.getElementById("password")
+
+      if(passw.type === "password"){
+            passw.type = "text"
+            document.getElementById("show").style.display = "none"
+            document.getElementById("hide").style.display = "inline"
+        }
+        else{
+            passw.type = "password"
+            document.getElementById("show").style.display = "inline"
+            document.getElementById("hide").style.display = "none"
+        }
+}
+
+function fun(){
+        var  word= document.getElementById("password").value;
+        var firstLetter = word.charAt(0)
+
+        if(word.length === 0){
+            document.getElementById("first").style.color = "black"
+            document.getElementById("minimum").style.color = "black"
+            document.getElementById("special").style.color = "black"
+            document.getElementById("number").style.color = "black"
+        }
+        if(firstLetter === firstLetter.toUpperCase()){
+            document.getElementById("first").innerHTML= "First letter is capital"
+            document.getElementById("first").style.color = "green"
+        }
+        else{
+            document.getElementById("first").innerHTML= "First letter is capital"
+            document.getElementById("first").style.color = "red"
+        }
+
+        var specials = ["@","#","$","%","*","&"]
+
+        if(specials.some(a=>word.includes(a))){
+            document.getElementById("special").innerHTML= "Atleast one special character"
+            document.getElementById("special").style.color = "green"
+        }
+        else{
+            document.getElementById("special").innerHTML= "Atleast one special character"
+            document.getElementById("special").style.color = "red"
+        }
+
+        var num = ["0","1","2","3","4","5","6","7","8","9"]
+
+        if(num.some(a=>word.includes(a))){
+            document.getElementById("number").innerHTML= " Atleast one number"
+            document.getElementById("number").style.color = "green"
+        }
+        else{
+            document.getElementById("number").innerHTML= "Atleast one number"
+            document.getElementById("number").style.color = "red"
+        }
+        
+        if(word.length>=8){
+            document.getElementById("minimum").innerHTML= "Minimum 8 characters"
+            document.getElementById("minimum").style.color = "green"
+        }
+        else{
+            document.getElementById("minimum").innerHTML= "Minimum 8 characters"
+            document.getElementById("minimum").style.color = "red"
+        }
+    }
+
+function Showproduct(){
+    var content =""
+    total_product.map((value)=>{
+        content +=`
+        <div class="col-lg-3 col-md-6 col-12 mt-3">
+            <div class="card">
+                <img src="${value.img}"  height="200"  alt="">
+                <div class="card-body">
+                    <h5>${value.name}</h5>
+                    <p>Price :₹${value.price}</p>
+                    <button class="btn btn-primary w-100" onclick="Addto(${value.id})">Add To Cart</button>
+                </div>
+            </div>
+        </div>
+        `
+    })
+    document.getElementById("CardRow").innerHTML=content
+} 
+Showproduct()
+
+var cart=[]
+var count = 0;
+var total_price=0;
+
+function cart_count(){
+    document.getElementById("carts").innerHTML=count;
+    document.getElementById("carts").style.display =count === 0 ? "none" : "inline";
+}
+
+function Addto(valueID){
+
+    var data = total_product.find(a=>a.id === valueID);
+
+    var existingProduct= cart.find(a=>a.id === valueID)
+    if(existingProduct){
+        existingProduct.quantity++
+    }
+    else{
+        data.quantity = 1
+        cart.push(data)
+    }
+    
+    count++;
+    cart_count()
+
+    
+    ShowCart(cart)
+    total_price = total_price+data.price
+    document.getElementById("totalprices").innerHTML = `Total Price:${total_price}`
+
+    
+}
+ 
+function  ShowCart(cData){
+     var content=""
+     if(cData.length === 0){
+        content+=`
+        <tr>
+            <td  colspan="5"class="text-danger ps-5 text-center">Your Cart is Empty!</td>
+        </tr>
+     `
+     }
+     else{
+        cData.map((value)=>{
+            content+=`
+            <tr>
+                <td>
+                  <img src="${value.img}"  height="50" width="50" alt="">
+                </td>
+                <td>${value.name}</td>
+                <td>${value.quantity}</td>
+                <td>${value.price}</td>
+                <td><button class="btn btn-danger" onclick="RemoveCart(${value.id})">Remove</button></td>
+              </tr>
+            `
+        })
+     }
+     document.getElementById("cart-remove").innerHTML=content;      
+}
+
+function RemoveCart(valueID){
+    var product = total_product.find(a=>a.id === valueID)
+    if(product.quantity>1){
+        product.quantity--
+
+    }
+    else{
+        cart = cart.filter(a=>a.id !== valueID)
+    }
+    count--;
+    cart_count()
+
+    total_price = total_price - product.price
+    document.getElementById("totalprices").innerHTML = total_price === 0? "" :`Total Price:${total_price}`
+
+
+    ShowCart(cart)
+}
+
+// =================================================SEARCH FUCTION START====================================
+
+const displayCard=(data)=>{
+    var content1=""
+    data.map((value)=>{
+        content1+=`
+        <div class="col-lg-3 col-md-4 col-12 mt-3 ">
+            <div class="card">
+             <img src="${value.img}" height="200" alt="">
+             <div class="card-body">
+                <h5>${value.name}</h5>
+                <p>${value.price}</p>
+             </div>
+            </div>
+        </div>  
+        `
+    })
+    document.getElementById("Searchrow").innerHTML=content1
+}
+displayCard(total_product)
+const fun1=(event)=>{
+    event.preventDefault()
+    var search = document.getElementById("result").value.toLowerCase();
+    var input = total_product.filter(a=>a.name.toLowerCase().includes(search) || a.price.toString().includes(search))
+    displayCard(input)
+}
